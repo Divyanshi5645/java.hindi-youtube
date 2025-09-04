b2b54a63d7bc1193e61120eae0e4680b61d1a938
Divyanshi5645 <divyanshiparashar2@gmail.com>
2025-09-04 17:23:17 +0000

    variable

--- .devcontainer/01_basics/01_variable.js
+++ .devcontainer/01_basics/01_variable.js
@@ -1,1 +1,19 @@
+const accountId=3456
+let accountEmail ="divya@goole.com"
+var accountPassowrd ="3456"
+accountCity ="jodhpur"
+let accountState;
 
+// accountId = 2 // not allowed
+
+accountEmail="dp@dp.com"
+accountPassowrd="123456"
+accountCity="chennai"
+console.log(accountId);
+
+/*
+prefer not use var
+bz issue of block scope and functional scope 
+ */
+
+console.table([accountId, accountEmail,  accountPassowrd,    accountCity, accountState])
